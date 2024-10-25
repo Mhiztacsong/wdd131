7bd8285c1eb1f890fe995e08ffb0797a78746ab9
Mhiztacsong <joh24038@byui.edu>
2024-10-25 17:17:45 +0100

    final project

--- final-project/scripts/scripts.js
+++ final-project/scripts/scripts.js
@@ -36,3 +36,21 @@ document.addEventListener('DOMContentLoaded', () => {
 if (document.getElementById('resource-list')) {
     loadResources();
 }
+
+// Hamburger
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+
+hamButton.addEventListener("click", () => {
+    navigation.classList.toggle("open");
+    hamButton.classList.toggle("open");
+
+});
+
+const currentYear = new Date().getFullYear();
+
+document.getElementById("currentyear").textContent = currentYear;
+
+const lastModifiedDate = document.lastModified;
+
+document.getElementById("lastModified").textContent = `Last Modification: ${lastModifiedDate}`;
